7f0fdab01f08f19ed46255e60c231ac7e8c8f035
truonghoi1205 <phamtruonghoi.dmx@gmail.com>
2025-06-03 22:07:56 +0700

    fix reload video

--- src/components/home/SecondContentSection.jsx
+++ src/components/home/SecondContentSection.jsx
@@ -2,8 +2,21 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import {useEffect, useRef, useState} from "react";
 
 const SecondContentSection = () => {
+
+    const videoRef = useRef(null);
+    const [videoSrc, setVideoSrc] = useState(null);
+
+    useEffect(() => {
+        fetch("/video/demo.mp4")
+            .then(res => res.blob())
+            .then(blob => {
+                const url = URL.createObjectURL(blob);
+                setVideoSrc(url);
+            });
+    }, []);
     return (
         <section className="bg-gradient-to-r from-white to-[#bc9a5c] py-12 ">
             <div className="max-w-7xl mx-auto px-4 sm:px-6">
@@ -19,13 +32,14 @@ const SecondContentSection = () => {
                 </div>
                 <div className="relative w-full max-w-4xl mx-auto aspect-video rounded-lg shadow-xl overflow-hidden">
                     <video
+                        ref={videoRef}
                         autoPlay
                         muted
                         playsInline
                         controls
                         className="absolute top-0 left-0 w-full h-full object-cover"
+                        src={videoSrc}
                     >
-                        <source src="/video/demo.mp4" type="video/mp4"/>
                         Trình duyệt của bạn không hỗ trợ video.
                     </video>
                 </div>
